eaaf962cda59c371822347af8de56cd7ec630d4f
Van Vreckem <svanvreckem@helb-prigogine.be>
2021-06-01 21:14:57 +0200

    refactor: renamed properties

--- Models/Trame.cs
+++ Models/Trame.cs
@@ -1,29 +1,25 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace MeteoSationProject.Models
 {
-    class Trame
+    public class Trame
     {
-        public byte ID { get; set; }
-        public byte DataNbr { get; set; }
-        public byte CheckSum { get; set; }
-        public UInt32[] Datas { get; set; }
-        public byte Type { get; set; }
+        public byte _id { get; set; }
+        public byte _dataNumber { get; set; }
+        public byte _checkSum { get; set; }
+        public UInt32[] _datas { get; set; }
+        public byte _type { get; set; }
 
-        public Trame(byte iD, byte dataNbr, byte type, byte checkSum, UInt32[] datas)
+        public Trame(byte iD, byte dataNumber, byte type, byte checkSum, UInt32[] datas)
         {
-            ID = iD;
-            DataNbr = dataNbr;
-            CheckSum = checkSum;
-            Datas = datas;
-            Type = type;
+            _id = iD;
+            _dataNumber = dataNumber;
+            _checkSum = checkSum;
+            _datas = datas;
+            _type = type;
         }
 
-        override public String ToString()
+        /*override public String ToString()
         {
             String features = "Id: " + ID + ", DataNbr: " + DataNbr + ", Type:" + Type + ", Datas: ";
 
@@ -32,7 +28,7 @@ namespace MeteoSationProject.Models
             features += ", CheckSum: " + CheckSum;
 
             return features;
-        }
+        }*/
 
     }
 }
