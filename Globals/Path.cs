ababb53fc0890d07a25bc3c4b6a24ad02fb799dd
Van Vreckem <svanvreckem@helb-prigogine.be>
2021-06-01 21:14:45 +0200

    feat: globals path

---
+++ Globals/Path.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MeteoSationProject.Globals
+{
+    class Path
+    {
+
+        public static readonly string ROOT_PATH = Directory.GetParent(System.AppDomain.CurrentDomain.BaseDirectory).Parent.Parent.FullName;
+
+    }
+}
