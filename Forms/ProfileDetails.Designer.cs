deccc15e4244de0b0147a8b8e62e15852925b3ec
Van Vreckem <svanvreckem@helb-prigogine.be>
2021-06-02 18:47:39 +0200

    feat: showing the details of the connected user

---
+++ Forms/ProfileDetails.Designer.cs
@@ -0,0 +1,249 @@
+﻿namespace MeteoSationProject.Forms
+{
+    partial class ProfileDetails
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lbUsername = new System.Windows.Forms.Label();
+            this.lbPassword = new System.Windows.Forms.Label();
+            this.lbAccessId = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.checkCreateId = new System.Windows.Forms.CheckBox();
+            this.checkDestroyId = new System.Windows.Forms.CheckBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.checkConfigAlarm = new System.Windows.Forms.CheckBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.checkAddUser = new System.Windows.Forms.CheckBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(64, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Username: ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(64, 84);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Password:";
+            // 
+            // lbUsername
+            // 
+            this.lbUsername.AutoSize = true;
+            this.lbUsername.Location = new System.Drawing.Point(131, 49);
+            this.lbUsername.Name = "lbUsername";
+            this.lbUsername.Size = new System.Drawing.Size(35, 13);
+            this.lbUsername.TabIndex = 2;
+            this.lbUsername.Text = "label3";
+            // 
+            // lbPassword
+            // 
+            this.lbPassword.AutoSize = true;
+            this.lbPassword.Location = new System.Drawing.Point(131, 84);
+            this.lbPassword.Name = "lbPassword";
+            this.lbPassword.Size = new System.Drawing.Size(35, 13);
+            this.lbPassword.TabIndex = 3;
+            this.lbPassword.Text = "label4";
+            // 
+            // lbAccessId
+            // 
+            this.lbAccessId.AutoSize = true;
+            this.lbAccessId.Location = new System.Drawing.Point(131, 114);
+            this.lbAccessId.Name = "lbAccessId";
+            this.lbAccessId.Size = new System.Drawing.Size(35, 13);
+            this.lbAccessId.TabIndex = 5;
+            this.lbAccessId.Text = "label4";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(63, 114);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(57, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Access Id:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(83, 144);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(79, 25);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Rights";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(67, 181);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(79, 13);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Allow create id:";
+            // 
+            // checkCreateId
+            // 
+            this.checkCreateId.AutoSize = true;
+            this.checkCreateId.Enabled = false;
+            this.checkCreateId.Location = new System.Drawing.Point(149, 181);
+            this.checkCreateId.Name = "checkCreateId";
+            this.checkCreateId.Size = new System.Drawing.Size(15, 14);
+            this.checkCreateId.TabIndex = 8;
+            this.checkCreateId.UseVisualStyleBackColor = true;
+            // 
+            // checkDestroyId
+            // 
+            this.checkDestroyId.AutoSize = true;
+            this.checkDestroyId.Enabled = false;
+            this.checkDestroyId.Location = new System.Drawing.Point(149, 205);
+            this.checkDestroyId.Name = "checkDestroyId";
+            this.checkDestroyId.Size = new System.Drawing.Size(15, 14);
+            this.checkDestroyId.TabIndex = 10;
+            this.checkDestroyId.UseVisualStyleBackColor = true;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(63, 205);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(83, 13);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "Allow destroy id:";
+            // 
+            // checkConfigAlarm
+            // 
+            this.checkConfigAlarm.AutoSize = true;
+            this.checkConfigAlarm.Enabled = false;
+            this.checkConfigAlarm.Location = new System.Drawing.Point(149, 230);
+            this.checkConfigAlarm.Name = "checkConfigAlarm";
+            this.checkConfigAlarm.Size = new System.Drawing.Size(15, 14);
+            this.checkConfigAlarm.TabIndex = 12;
+            this.checkConfigAlarm.UseVisualStyleBackColor = true;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(52, 230);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(95, 13);
+            this.label7.TabIndex = 11;
+            this.label7.Text = "Allow config alarm:";
+            // 
+            // checkAddUser
+            // 
+            this.checkAddUser.AutoSize = true;
+            this.checkAddUser.Enabled = false;
+            this.checkAddUser.Location = new System.Drawing.Point(149, 253);
+            this.checkAddUser.Name = "checkAddUser";
+            this.checkAddUser.Size = new System.Drawing.Size(15, 14);
+            this.checkAddUser.TabIndex = 14;
+            this.checkAddUser.UseVisualStyleBackColor = true;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(49, 252);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(99, 13);
+            this.label8.TabIndex = 13;
+            this.label8.Text = "Allow user creation:";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(77, 9);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(85, 25);
+            this.label9.TabIndex = 15;
+            this.label9.Text = "Details";
+            // 
+            // ProfileDetails
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(246, 284);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.checkAddUser);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.checkConfigAlarm);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.checkDestroyId);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.checkCreateId);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lbAccessId);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lbPassword);
+            this.Controls.Add(this.lbUsername);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "ProfileDetails";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "ProfileDetails";
+            this.Load += new System.EventHandler(this.ProfileDetails_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lbUsername;
+        private System.Windows.Forms.Label lbPassword;
+        private System.Windows.Forms.Label lbAccessId;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.CheckBox checkCreateId;
+        private System.Windows.Forms.CheckBox checkDestroyId;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.CheckBox checkConfigAlarm;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.CheckBox checkAddUser;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+    }
+}
---
+++ Forms/ProfileDetails.cs
@@ -0,0 +1,36 @@
+﻿using MeteoSationProject.Models;
+using MeteoSationProject.Services.UserProvider;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MeteoSationProject.Forms
+{
+    public partial class ProfileDetails : Form
+    {
+        public ProfileDetails()
+        {
+            InitializeComponent();
+            MaximizeBox = false;
+        }
+
+        private void ProfileDetails_Load(object sender, EventArgs e)
+        {
+            User user = UserProvider.User;
+            lbUsername.Text = user._username;
+            lbPassword.Text = user._password;
+            lbAccessId.Text = user._accessId.ToString();
+
+            checkCreateId.Checked = user.Access._allowCreateId;
+            checkDestroyId.Checked = user.Access._allowDestroyId;
+            checkConfigAlarm.Checked = user.Access._allowConfigAlarm;
+            checkAddUser.Checked = user.Access._userCreation;
+        }
+    }
+}
