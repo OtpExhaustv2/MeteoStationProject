83df38114421072bd9ebe6199c98eed2dfef2a81
Van Vreckem <svanvreckem@helb-prigogine.be>
2021-06-02 18:47:27 +0200

    feat: data models

---
+++ Models/Access.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MeteoSationProject.Models
+{
+    class Access
+    {
+
+        public string _label { get; }
+        public bool _allowCreateId { get; }
+        public bool _allowDestroyId { get; }
+        public bool _allowConfigAlarm { get; }
+        public bool _userCreation { get; }
+
+        public Access(string label, bool allowCreateId, bool allowDestroyId, bool allowConfigAlarm, bool userCreation)
+        {
+            _label = label;
+            _allowCreateId = allowCreateId;
+            _allowDestroyId = allowDestroyId;
+            _allowConfigAlarm = allowConfigAlarm;
+            _userCreation = userCreation;
+        }
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MeteoSationProject.Models
+{
+    class User
+    {
+
+        public int _id { get; set; }
+        public string _username { get; set; }
+        public string _password { get; set; }
+        public int _accessId { get; set; }
+        public Access _access { get; set; }
+
+        public User(int id, string username, string password, int accessId, Access access)
+        {
+            _id = id;
+            _username = username;
+            _password = password;
+            _accessId = accessId;
+            _access = access;
+        }
+
+        public Access Access
+        {
+            get { return _access; }
+        }
+    }
+}
