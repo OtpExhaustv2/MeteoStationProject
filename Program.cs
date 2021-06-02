3f11c34256b6b9d75be36685fd96c87b0f26eeda
Van Vreckem <svanvreckem@helb-prigogine.be>
2021-06-02 18:46:13 +0200

    refactor: changed the main form to display on launch

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MeteoSationProject.Forms;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -16,7 +17,7 @@ namespace MeteoSationProject
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new MeteoStationController());
+            Application.Run(new LoginController());
         }
     }
 }
