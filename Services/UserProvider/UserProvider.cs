3df21632ed236225d22e41f4842e7a14b368a936
Van Vreckem <svanvreckem@helb-prigogine.be>
2021-06-02 18:47:53 +0200

    feat: retrieve the current user

---
+++ Services/UserProvider/UserProvider.cs
@@ -0,0 +1,16 @@
+﻿using MeteoSationProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MeteoSationProject.Services.UserProvider
+{
+    class UserProvider
+    {
+        public static User User { get; set; }
+
+    }
+}
